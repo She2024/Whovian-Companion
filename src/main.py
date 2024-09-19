4aecd26f51d1182531eed08caa00d2f7376e65ee
she2024 <bullybluedog@hotmail.com>
2024-09-19 18:19:25 +1000

    correct spelling in the code for main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from init import db, ma, bcrypt, jwt
 
 # Application factories
 
-def creeate_app():
+def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
