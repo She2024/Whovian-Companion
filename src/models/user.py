95a3a9c0bd05486f38bac6a724baace286904e36
she2024 <bullybluedog@hotmail.com>
2024-09-23 21:09:40 +1000

    created user table and correct models folder name and VARCHAR condition in user class to solve import errors function now working'

--- src/models/user.py
+++ src/models/user.py
@@ -6,9 +6,9 @@ class User (db.Model):
 
     # attributes of table
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.varchar(15))
-    email = db.Column(db.varchar(100), nullable=False, unique=True)
-    password = db.Column(db.varchar(15), nullable=False)
+    name = db.Column(db.VARCHAR(15))
+    email = db.Column(db.VARCHAR(100), nullable=False, unique=True)
+    password = db.Column(db.VARCHAR(15), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 class UserSchema(ma.Schema):
